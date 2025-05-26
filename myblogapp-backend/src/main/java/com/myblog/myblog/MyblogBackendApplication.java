c69206700a1fcd6b466760f9d4377f02ee1593cb
bruno <bruno.krizic1999@gmail.com>
2025-05-26 21:17:53 +0200

    backend

---
+++ myblogapp-backend/src/main/java/com/myblog/myblog/MyblogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogBackendApplication.class, args);
+	}
+
+}
